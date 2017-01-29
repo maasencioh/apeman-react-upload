ae2bb342645d8a5e14a10b68875c7cbc44cc00a7
Taka Okunishi <okunishinishi@gmail.com>
2017-01-29 12:15:13 +0900

    Deploy Github pages

---
+++ demo/demo.component.jsx
@@ -0,0 +1,45 @@
+'use strict'
+
+import React from 'react'
+import ApUpload from '../../lib/ap_upload'
+import ApUploadStyle from '../../lib/ap_upload_style'
+import {ApImageStyle} from 'apeman-react-image'
+import {ApSpinnerStyle} from 'apeman-react-spinner'
+import {ApButtonStyle} from 'apeman-react-button'
+
+const DEMO_IMAGES = [
+  'https://raw.githubusercontent.com/apeman-asset-labo/apeman-asset-images/master/dist/dummy/12.jpg'
+]
+
+const Demo = React.createClass({
+  render () {
+    const s = this
+    return (
+      <div>
+        <ApSpinnerStyle />
+        <ApButtonStyle highlightColor='#b35600'/>
+        <ApImageStyle />
+        <ApUploadStyle />
+        <ApUpload multiple={ true }
+                  id='demo-file-upload-01'
+                  name='file-input-01'
+                  accept='image/*'
+                  onLoad={ s.handleLoaded }>
+        </ApUpload>
+
+        <ApUpload multiple={ true }
+                  id='demo-file-upload-02'
+                  name='file-input-02'
+                  accept='image/*'
+                  value={ DEMO_IMAGES }
+                  onLoad={ s.handleLoaded }>
+        </ApUpload>
+      </div>
+    )
+  },
+  handleLoaded (ev) {
+    console.log('result', ev.target, ev.urls)
+  }
+})
+
+export default Demo
